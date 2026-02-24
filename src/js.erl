330bdad910ee1a14aa865fc795a161c101c3ab06
Peter Lemenkov <lemenkov@gmail.com>
2026-02-24 21:39:56 +0100

    Switch to jsx
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/js.erl
+++ src/js.erl
@@ -1,17 +1,9 @@
 %% @author Kevin Smith <ksmith@basho.com>
 %% @copyright 2009-2010 Basho Technologies
 %%
-%%    Licensed under the Apache License, Version 2.0 (the "License");
-%%    you may not use this file except in compliance with the License.
-%%    You may obtain a copy of the License at
-%%
-%%        http://www.apache.org/licenses/LICENSE-2.0
-%%
-%%    Unless required by applicable law or agreed to in writing, software
-%%    distributed under the License is distributed on an "AS IS" BASIS,
-%%    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-%%    See the License for the specific language governing permissions and
-%%    limitations under the License.
+%% SPDX-FileCopyrightText: 2009-2010 Basho Technologies
+%% SPDX-FileCopyrightText: 2020-2026 Peter Lemenkov <lemenkov@gmail.com>
+%% SPDX-License-Identifier: Apache-2.0
 
 %% @doc Convenience module for interacting with Javascript from Erlang.
 %% The functions provided by this module marshal bindings and function
@@ -24,57 +16,71 @@
 
 -export([define/2, define/3, eval/2, call/3, call/4]).
 
-%% @spec define(reference(), binary()) -> ok | {error, any()}
 %% @doc Define one or more Javascript expressions.
+-spec define(reference(), binary()) -> ok | {error, any()}.
 define(Ctx, Js) ->
     define(Ctx, Js, []).
 
-%% @spec define(reference(), binary(), list(any())) -> ok | {error, any()}
-%% @doc Define one or more Javascript expressions using a set of bindings. Bindings
-%% are useful when the expressions use closures.
+%% @doc Define one or more Javascript expressions using a set of bindings.
+%% Bindings are useful when the expressions use closures.
+-spec define(reference(), binary(), list({binary() | atom(), any()})) -> ok | {error, any()}.
 define(Ctx, Js, Bindings) ->
     JsBindings = list_to_binary(build_bindings(Bindings, [])),
     FinalJs = iolist_to_binary([JsBindings, Js]),
     js_driver:define_js(Ctx, FinalJs).
 
-%% @spec eval(reference(), binary()) -> {ok, any()} | {error, any()}
-%% @doc Evaluate one or more Javascript expressions and return the results
+%% @doc Evaluate one or more Javascript expressions and return the results.
+-spec eval(reference(), binary()) -> {ok, any()} | {error, any()}.
 eval(Ctx, Js) ->
     js_driver:eval_js(Ctx, Js).
 
-%% @spec call(reference(), binary(), list(any())) -> {ok, Result} | {error, any()}
-%% @doc Call a function by name with a list of arguments. This is roughly the
-%% same as apply in most other languages.
+%% @doc Call a function by name with a list of arguments.
+%% This is roughly the same as apply in most other languages.
+-spec call(reference(), binary(), list(any())) -> {ok, any()} | {error, any()}.
 call(Ctx, FunctionName, Args) ->
     call(Ctx, FunctionName, Args, []).
 
-%% @spec call(reference(), binary(), list(any()), list(any())) -> {ok, Result} | {error, any()}
-%% @doc Call a function by name with a list of arguments and environmental bindings. Bindings
-%% behave just like define/3.
+%% @doc Call a function by name with a list of arguments and environmental
+%% bindings. Bindings behave just like define/3.
+-spec call(reference(), binary(), list(any()), list({binary() | atom(), any()})) ->
+    {ok, any()} | {error, any()}.
 call(Ctx, FunctionName, Args, Bindings) ->
     JsBindings = list_to_binary(build_bindings(Bindings, [])),
     ArgList = build_arg_list(Args, []),
     EscapedFunctionName = binary:replace(FunctionName, <<"\"">>, <<"\\\"">>, [global]),
-    Js = iolist_to_binary([<<"function() {">>, JsBindings, <<" if (">>, FunctionName, <<" === undefined) { throw(\"">>,
-                           EscapedFunctionName, <<" not defined\"); } ">>,
-                           <<"return ">>, FunctionName, <<"(">>, ArgList, <<");">>, <<"}();">>]),
+    Js = iolist_to_binary([
+        <<"function() {">>,
+        JsBindings,
+        <<" if (">>,
+        FunctionName,
+        <<" === undefined) { throw(\"">>,
+        EscapedFunctionName,
+        <<"\" not defined\"); } ">>,
+        <<"return ">>,
+        FunctionName,
+        <<"(">>,
+        ArgList,
+        <<");">>,
+        <<"}();">>
+    ]),
     js_driver:eval_js(Ctx, Js).
 
 %% Internal functions
 build_bindings([], Accum) ->
     Accum;
-build_bindings([{VarName, Value}|T], Accum) ->
-    FinalVarName = case is_atom(VarName) of
-                       true ->
-                           atom_to_list(VarName);
-                       false ->
-                           VarName
-                   end,
-    build_bindings(T, [[FinalVarName, "=", mochijson2:encode(Value), ";"]|Accum]).
+build_bindings([{VarName, Value} | T], Accum) ->
+    FinalVarName =
+        case is_atom(VarName) of
+            true ->
+                atom_to_list(VarName);
+            false ->
+                VarName
+        end,
+    build_bindings(T, [[FinalVarName, "=", jsx:encode(Value), ";"] | Accum]).
 
 build_arg_list([], Accum) ->
     lists:reverse(Accum);
-build_arg_list([H|[]], Accum) ->
-    build_arg_list([], [mochijson2:encode(H)|Accum]);
-build_arg_list([H|T], Accum) ->
-    build_arg_list(T, [[mochijson2:encode(H), ","]|Accum]).
+build_arg_list([H | []], Accum) ->
+    build_arg_list([], [jsx:encode(H) | Accum]);
+build_arg_list([H | T], Accum) ->
+    build_arg_list(T, [[jsx:encode(H), ","] | Accum]).
--- test/eval_tests.erl
+++ test/eval_tests.erl
@@ -1,106 +1,233 @@
+%% SPDX-FileCopyrightText: 2009-2010 Basho Technologies
+%% SPDX-FileCopyrightText: 2020-2026 Peter Lemenkov <lemenkov@gmail.com>
+%% SPDX-License-Identifier: Apache-2.0
+
 -module(eval_tests).
 
 -include_lib("eunit/include/eunit.hrl").
 
 basic_js_test_() ->
-	{setup,
-		fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
-		fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
-		fun(Handle) -> [
-			% Basic variable testing
-			{"define variable", ?_assertEqual(ok, js:define(Handle, <<"var x = 100;">>))},
-			{"evaluate variable", ?_assertEqual({ok, 100}, js:eval(Handle, <<"x;">>))},
-			{"define empty data", ?_assertEqual(ok, js:define(Handle, ""))},
+    {setup,
+        fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
+        fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
+        fun(Handle) ->
+            [
+                %% Basic variable testing
+                {"define variable", ?_assertEqual(ok, js:define(Handle, <<"var x = 100;">>))},
+                {"evaluate variable", ?_assertEqual({ok, 100}, js:eval(Handle, <<"x;">>))},
+                {"define empty data", ?_assertEqual(ok, js:define(Handle, ""))},
 
-			% Basic function testing
-			{"define fun add_two", ?_assertEqual(ok, js:define(Handle, <<"function add_two(x, y) { return x + y; };">>))},
-			{"use fun add_two for numbers", ?_assertEqual({ok, 95}, js:call(Handle, <<"add_two">>, [85, 10]))},
-			{"use fun add_two for strings", ?_assertEqual({ok, <<"testing123">>}, js:call(Handle, <<"add_two">>, [<<"testing">>, <<"123">>]))},
+                %% Basic function testing
+                {"define fun add_two",
+                    ?_assertEqual(
+                        ok,
+                        js:define(Handle, <<"function add_two(x, y) { return x + y; };">>)
+                    )},
+                {"use fun add_two for numbers",
+                    ?_assertEqual({ok, 95}, js:call(Handle, <<"add_two">>, [85, 10]))},
+                {"use fun add_two for strings",
+                    ?_assertEqual(
+                        {ok, <<"testing123">>},
+                        js:call(Handle, <<"add_two">>, [<<"testing">>, <<"123">>])
+                    )},
 
-			{"define subtract fun as var", ?_assertEqual(ok, js:define(Handle, <<"var f = function(x, y) \n{ return y - x; \n};">>))},
-			{"use subtract fun", ?_assertEqual({ok, 75}, js:call(Handle, <<"f">>, [10, 85]))},
+                {"define subtract fun as var",
+                    ?_assertEqual(
+                        ok,
+                        js:define(
+                            Handle,
+                            <<"var f = function(x, y) \n{ return y - x; \n};">>
+                        )
+                    )},
+                {"use subtract fun",
+                    ?_assertEqual({ok, 75}, js:call(Handle, <<"f">>, [10, 85]))},
 
-			{"define JSON extraction fun", ?_assertEqual(ok, js:define(Handle, <<"function get_first(data) { return data[\"first\"]; };">>))},
-			{"use JSON exctaction fun", ?_assertEqual({ok, <<"abc">>}, js:call(Handle, <<"get_first">>, [{struct, [{<<"first">>, <<"abc">>}]}]))},
+                {"define JSON extraction fun",
+                    ?_assertEqual(
+                        ok,
+                        js:define(
+                            Handle,
+                            <<"function get_first(data) { return data[\"first\"]; };">>
+                        )
+                    )},
+                {"use JSON extraction fun",
+                    ?_assertEqual(
+                        {ok, <<"abc">>},
+                        js:call(Handle, <<"get_first">>, [#{<<"first">> => <<"abc">>}])
+                    )},
 
-			%% Regression test case for embedded error properties in function return values
-			{"define JSON returning fun", ?_assertEqual(ok, js:define(Handle, <<"function return_error_property() { return [{\"value\": \"some_value\", \"list\": [{\"error\": \"some_error\"}]}]; }">>))},
-			{"use JSON returning fun", ?_assertEqual({ok,[{struct,[{<<"value">>,<<"some_value">>},{<<"list">>,[{struct,[{<<"error">>,<<"some_error">>}]}]}]}]}, js:call(Handle, <<"return_error_property">>, []))},
+                %% Regression test case for embedded error properties in function return values
+                {"define JSON returning fun",
+                    ?_assertEqual(
+                        ok,
+                        js:define(
+                            Handle,
+                            <<"function return_error_property() { return [{\"value\": \"some_value\", \"list\": [{\"error\": \"some_error\"}]}]; }">>
+                        )
+                    )},
+                {"use JSON returning fun",
+                    ?_assertEqual(
+                        {ok, [
+                            #{
+                                <<"value">> => <<"some_value">>,
+                                <<"list">> => [#{<<"error">> => <<"some_error">>}]
+                            }
+                        ]},
+                        js:call(Handle, <<"return_error_property">>, [])
+                    )},
 
-			%% Regression test case for github issue basho/erlang_js#42 - quotes in anonymous functions
-			{"define anonymous fun with quotes", ?_assertEqual({ok, [<<"foo">>, <<"bar">>]}, js:call(Handle, <<"function(x) { return x.split(\" \"); }">>, [<<"foo bar">>]))}
-		] end
-      }.
+                %% Regression test case for github issue basho/erlang_js#42 - quotes in anonymous functions
+                {"define anonymous fun with quotes",
+                    ?_assertEqual(
+                        {ok, [<<"foo">>, <<"bar">>]},
+                        js:call(Handle, <<"function(x) { return x.split(\" \"); }">>, [
+                            <<"foo bar">>
+                        ])
+                    )}
+            ]
+        end}.
 
 binding_test_() ->
-	{setup,
-		fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
-		fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
-		fun(Handle) -> [
-			{"define fun1 with bindings in place", ?_assertEqual(ok, js:define(Handle, <<"var c = 100;function constant_mult(x) { return x * c; }">>))},
-			{"eval fun1 using default val", ?_assertEqual({ok, 200}, js:call(Handle, <<"constant_mult">>, [2]))},
-			{"eval fun1 using other val", ?_assertEqual({ok, 1000}, js:call(Handle, <<"constant_mult">>, [2], [{<<"c">>, 500}]))},
-			{"define fun2 with bindings", ?_assertEqual(ok, js:define(Handle, <<"function constant_div(x) { return x / q; }">>, [{<<"q">>, 2}]))},
-			{"eval fun2 using default val", ?_assertEqual({ok, 5}, js:call(Handle, <<"constant_div">>, [10]))},
-			{"eval fun2 using other val", ?_assertEqual({ok, 3}, js:call(Handle, <<"constant_div">>, [9], [{<<"q">>, 3}]))}
-		] end
-      }.
+    {setup,
+        fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
+        fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
+        fun(Handle) ->
+            [
+                {"define fun1 with bindings in place",
+                    ?_assertEqual(
+                        ok,
+                        js:define(
+                            Handle,
+                            <<"var c = 100;function constant_mult(x) { return x * c; }">>
+                        )
+                    )},
+                {"eval fun1 using default val",
+                    ?_assertEqual({ok, 200}, js:call(Handle, <<"constant_mult">>, [2]))},
+                {"eval fun1 using other val",
+                    ?_assertEqual(
+                        {ok, 1000},
+                        js:call(Handle, <<"constant_mult">>, [2], [{<<"c">>, 500}])
+                    )},
+                {"define fun2 with bindings",
+                    ?_assertEqual(
+                        ok,
+                        js:define(
+                            Handle,
+                            <<"function constant_div(x) { return x / q; }">>,
+                            [{<<"q">>, 2}]
+                        )
+                    )},
+                {"eval fun2 using default val",
+                    ?_assertEqual({ok, 5}, js:call(Handle, <<"constant_div">>, [10]))},
+                {"eval fun2 using other val",
+                    ?_assertEqual(
+                        {ok, 3},
+                        js:call(Handle, <<"constant_div">>, [9], [{<<"q">>, 3}])
+                    )}
+            ]
+        end}.
 
 charset_test_() ->
-	%% Kanji character
-	Kanji = <<123,34,116,101,120,116,34,58,34,228,188,141,34,125,10>>,
-	{setup,
-		fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
-		fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
-		fun(Handle) -> [
-			{"Define fun", ?_assertEqual(ok, js:define(Handle, <<"function foo(x) { return x; }">>))},
-			{"Check if this fun can return Kanji?", ?_assertEqual({ok, Kanji}, js:call(Handle, <<"foo">>, [Kanji]))}
-		] end
-      }.
+    %% Kanji character
+    Kanji = <<123, 34, 116, 101, 120, 116, 34, 58, 34, 228, 188, 141, 34, 125, 10>>,
+    {setup,
+        fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
+        fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
+        fun(Handle) ->
+            [
+                {"Define fun",
+                    ?_assertEqual(
+                        ok, js:define(Handle, <<"function foo(x) { return x; }">>)
+                    )},
+                {"Check if this fun can return Kanji?",
+                    ?_assertEqual({ok, Kanji}, js:call(Handle, <<"foo">>, [Kanji]))}
+            ]
+        end}.
 
 json_test_() ->
-	Struct1 = {struct, [{<<"test">>, <<"1">>}]},
-	Struct2 = {struct, [{<<"test2">>, 2}]},
-	[
-		{"Check if we can parse back and forth strings", ?_assertEqual(Struct1, mochijson2:decode(mochijson2:encode(Struct1)))},
-		{"Check if we can parse back and forth numbers", ?_assertEqual(Struct2, mochijson2:decode(mochijson2:encode(Struct2)))}
-	].
+    Map1 = #{<<"test">> => <<"1">>},
+    Map2 = #{<<"test2">> => 2},
+    [
+        {"Check if we can parse back and forth strings",
+            ?_assertEqual(Map1, jsx:decode(jsx:encode(Map1)))},
+        {"Check if we can parse back and forth numbers",
+            ?_assertEqual(Map2, jsx:decode(jsx:encode(Map2)))}
+    ].
 
 ejslog_test_() ->
-	FileName = "./mozjs_ejslog_test.log",
-	{setup,
-		fun() -> _ = file:delete(FileName), {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
-		fun(Handle) -> mozjs_nif:sm_stop(Handle), _ = file:delete(FileName) end,
-		fun(Handle) -> [
-			{"Call ejsLog", ?_assertEqual({ok, true}, js_driver:eval_js(Handle, iolist_to_binary([<<"ejsLog('">>, FileName, <<"', 'Hello')">>])))},
-			{"Ensure ejsLog created log-file", ?_assert(filelib:is_file(FileName))}
-		] end
-      }.
+    FileName = "./mozjs_ejslog_test.log",
+    {setup,
+        fun() ->
+            _ = file:delete(FileName),
+            {ok, Handle} = mozjs_nif:sm_init(8, 8),
+            Handle
+        end,
+        fun(Handle) ->
+            mozjs_nif:sm_stop(Handle),
+            _ = file:delete(FileName)
+        end,
+        fun(Handle) ->
+            [
+                {"Call ejsLog",
+                    ?_assertEqual(
+                        {ok, true},
+                        js_driver:eval_js(
+                            Handle,
+                            iolist_to_binary([<<"ejsLog('">>, FileName, <<"', 'Hello')">>])
+                        )
+                    )},
+                {"Ensure ejsLog created log-file", ?_assert(filelib:is_file(FileName))}
+            ]
+        end}.
 
 error_test_() ->
-	{setup,
-		fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
-		fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
-		fun(Handle) -> [
-			{"Bad syntax", ?_assert(verify_error(js:define(Handle, <<"functoin foo(x, y) { return true; };">>)))},
+    {setup,
+        fun() -> {ok, Handle} = mozjs_nif:sm_init(8, 8), Handle end,
+        fun(Handle) -> mozjs_nif:sm_stop(Handle) end,
+        fun(Handle) ->
+            [
+                {"Bad syntax",
+                    ?_assert(
+                        verify_error(
+                            js:define(Handle, <<"functoin foo(x, y) { return true; };">>)
+                        )
+                    )},
 
-		        {"Define fun with arity 2...", ?_assertEqual(ok, js:define(Handle, <<"function foo(x, y) { return true; };">>))},
-			{"..but give it 2 args and extra comma (valid)", ?_assertEqual({ok, true}, js:eval(Handle, <<"foo(100, 200, );">>))},
-			{"..and give it 3 args (also valid)", ?_assertEqual({ok, true}, js:eval(Handle, <<"foo(100, 200, 300);">>))},
+                {"Define fun with arity 2...",
+                    ?_assertEqual(
+                        ok,
+                        js:define(Handle, <<"function foo(x, y) { return true; };">>)
+                    )},
+                {"..but give it 2 args and extra comma (valid)",
+                    ?_assertEqual({ok, true}, js:eval(Handle, <<"foo(100, 200, );">>))},
+                {"..and give it 3 args (also valid)",
+                    ?_assertEqual({ok, true}, js:eval(Handle, <<"foo(100, 200, 300);">>))},
 
-			{"Define fun which throws error", ?_assertEqual(ok, js:define(Handle, <<"function foo() { throw new Error(\"notfound\"); };">>))},
-			{"Check if this fun actually returns error?", ?_assert(verify_error(js:call(Handle, <<"foo">>, [])))},
+                {"Define fun which throws error",
+                    ?_assertEqual(
+                        ok,
+                        js:define(
+                            Handle,
+                            <<"function foo() { throw new Error(\"notfound\"); };">>
+                        )
+                    )},
+                {"Check if this fun actually returns error?",
+                    ?_assert(verify_error(js:call(Handle, <<"foo">>, [])))},
 
-			{"Try to execute an empty string", ?_assert(verify_error(js:call(Handle, <<>>, [])))},
+                {"Try to execute an empty string",
+                    ?_assert(verify_error(js:call(Handle, <<>>, [])))},
 
-			{"Call for non-existing fun", ?_assert(verify_error(js:eval(Handle, <<"blah(\"wubba\");">>)))}
-		] end
-      }.
+                {"Call for non-existing fun",
+                    ?_assert(verify_error(js:eval(Handle, <<"blah(\"wubba\");">>)))}
+            ]
+        end}.
 
 %% Internal functions
-verify_error({error, [{<<"lineno">>, LineNo}, {<<"message">>, Msg}, {<<"source">>, Source}]}) when is_number(LineNo), is_binary(Msg), is_binary(Source) ->
+verify_error({error, #{<<"lineno">> := LineNo, <<"message">> := Msg, <<"source">> := Source}}) when
+    is_number(LineNo), is_binary(Msg), is_binary(Source)
+->
     true;
 verify_error(Error) ->
-    % Shoudn't normally happen.
+    %% Shouldn't normally happen.
     ?debugFmt("Error: ~p~n", [Error]),
     false.
